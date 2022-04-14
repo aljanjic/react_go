ecbad884f4d938fe59caf4290f8c8322c05a2653
Aljosa Janjic <janjic_aljosa@hotmail.com>
2022-04-14 11:36:20 +0200

    databases

---
+++ to-transfer/movies_202204141132.sql
@@ -0,0 +1,6 @@
+INSERT INTO public.movies (title,description,"year",release_date,runtime,rating,mpaa_rating,created_at,updated_at,poster) VALUES
+	 ('American Psycho',' A wealthy New York investment banking executive hides his alternate psychopathic ego',2000,'2000-04-14',102,4,'R','2021-05-17 00:00:00','2022-04-11 22:06:41.962456','/3ddHhfMlZHZCefHDeaP8FzSoH4Y.jpg'),
+	 ('The Dark Knight','The menace known as the Joker wreaks havoc on Gotham City',2008,'2008-07-18',152,5,'PG13','2021-05-17 00:00:00','2022-04-11 22:06:49.962301','/qJ2tW6WMUDux911r6m7haRef0WH.jpg'),
+	 ('The Shawshank Redemption','Two imprisoned men bond over a number of years',1994,'1994-10-14',142,5,'R','2021-05-17 00:00:00','2022-04-12 12:44:08.349439','/q6y0Go1tsGEsmtFryDOJo3dEmqu.jpg'),
+	 ('Up','Carl, an old widower, goes off on an adventure in his flying house in search of Paradise Falls, his wife''s dream destination.',2022,'2022-02-07',96,0,'PG13','2022-04-11 20:25:42.911588','2022-04-12 12:44:20.997434','/muIaHotSaSUQr0KZCIJOYQEe7y2.jpg'),
+	 ('The Godfather','The aging patriarch of an organized crime dynasty transfers control to his son...',1972,'1972-03-24',175,5,'R','2021-05-17 00:00:00','2022-04-13 12:01:04.254985','/n6PvMAKL66gavIFxOyRB6czAeQO.jpg');
